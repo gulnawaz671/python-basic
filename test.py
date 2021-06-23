6e26da873e27bbae704eaa83238621427a067470
gulnawaz671 <gulnawaz671@gmail.com>
2021-06-23 15:19:37 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+x="this is global variable"
+def function1():
+  x="this is local variable"
+  print(x)
+
+def function2():
+   ab="my name is khan"
+   print(ab)
+   
+def function3():
+   abc="my name is jamal"
+   print(abc)
+  
+def function4():
+   abc="your name is waheed"
+   print(abc)  
+function1()
+print(x)
+function2()
+function3()
+function4()
